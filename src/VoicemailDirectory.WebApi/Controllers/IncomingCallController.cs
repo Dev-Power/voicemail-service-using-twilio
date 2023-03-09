e204be4bc154c594d751ea79d2973a7b5ef0d670
dev <dev@example.com>
2023-03-09 11:14:56 +0000

    feat: controller and service implementation

--- src/VoicemailDirectory.WebApi/Controllers/IncomingCallController.cs
+++ src/VoicemailDirectory.WebApi/Controllers/IncomingCallController.cs
@@ -1,10 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
 
 namespace VoicemailDirectory.WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class IncomingCallController
+public class IncomingCallController : TwilioController
 {
-    
+    private readonly ILogger<IncomingCallController> _logger;
+    private readonly VoicemailOptions _voicemailOptions;
+
+    public IncomingCallController(
+        ILogger<IncomingCallController> logger,
+        IOptions<VoicemailOptions> voicemailOptions
+    )
+    {
+        _logger = logger;
+        _voicemailOptions = voicemailOptions.Value;
+    }
+
+    [HttpPost]
+    public TwiMLResult Index([FromForm] string from)
+    {
+        _logger.LogInformation("Incoming call from {callingNumber}", from);
+
+        var response = new VoiceResponse();
+
+        var redirectUrl = _voicemailOptions.Owners.Contains(from)
+            ? Url.Action("Index", "Directory")!
+            : Url.Action("Index", "Record")!;
+
+        response.Redirect(
+            url: new Uri(redirectUrl, UriKind.Relative),
+            method: Twilio.Http.HttpMethod.Post
+        );
+
+        return TwiML(response);
+    }
 }
--- src/VoicemailDirectory.WebApi/Controllers/RecordController.cs
+++ src/VoicemailDirectory.WebApi/Controllers/RecordController.cs
@@ -1,10 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+using VoicemailDirectory.WebApi.Services;
 
 namespace VoicemailDirectory.WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]/[action]")]
-public class RecordController
+public class RecordController : TwilioController
 {
-    
+    private readonly ILogger<IncomingCallController> _logger;
+    private readonly FileService _fileService;
+
+    public RecordController(
+        ILogger<IncomingCallController> logger,
+        FileService fileService
+    )
+    {
+        _logger = logger;
+        _fileService = fileService;
+    }
+
+    [HttpPost]
+    public TwiMLResult Index()
+    {
+        var response = new VoiceResponse();
+        response.Say("Hello, please leave a message after the beep.");
+        response.Record(
+            timeout: 10,
+            action: new Uri(Url.Action("RecordingStatus")!, UriKind.Relative),
+            method: Twilio.Http.HttpMethod.Post
+        );
+        return TwiML(response);
+    }
+
+    [HttpPost]
+    public async Task<TwiMLResult> RecordingStatus(
+        [FromForm] string recordingUrl,
+        [FromForm] string recordingSid
+    )
+    {
+        await _fileService.DownloadRecording(recordingUrl, recordingSid);
+
+        var response = new VoiceResponse();
+        response.Say("Thank you for leaving a message, goodbye.");
+        return TwiML(response);
+    }
 }
