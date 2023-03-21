83e3f5ff8aaa9faea05204eea087bf350e11a41b
dev <dev@example.com>
2023-03-21 13:31:05 +0300

    refactor: remove logging

--- src/VoicemailDirectory.WebApi/Controllers/IncomingCallController.cs
+++ src/VoicemailDirectory.WebApi/Controllers/IncomingCallController.cs
@@ -3,40 +3,38 @@ using Microsoft.Extensions.Options;
 using Twilio.AspNet.Core;
 using Twilio.TwiML;
 
+
 namespace VoicemailDirectory.WebApi.Controllers;
+
 
 [ApiController]
 [Route("[controller]")]
 public class IncomingCallController : TwilioController
 {
-    private readonly ILogger<IncomingCallController> _logger;
     private readonly VoicemailOptions _voicemailOptions;
 
-    public IncomingCallController(
-        ILogger<IncomingCallController> logger,
-        IOptionsSnapshot<VoicemailOptions> voicemailOptions
-    )
+
+    public IncomingCallController(IOptionsSnapshot<VoicemailOptions> voicemailOptions)
     {
-        _logger = logger;
         _voicemailOptions = voicemailOptions.Value;
     }
+
 
     [HttpPost]
     public TwiMLResult Index([FromForm] string from)
     {
-        _logger.LogInformation("Incoming call from {callingNumber}", from);
-
         var response = new VoiceResponse();
-
+        
         var redirectUrl = _voicemailOptions.Owners.Contains(from)
             ? Url.Action("Index", "Directory")!
             : Url.Action("Index", "Record")!;
+
 
         response.Redirect(
             url: new Uri(redirectUrl, UriKind.Relative),
             method: Twilio.Http.HttpMethod.Post
         );
-
+        
         return TwiML(response);
     }
 }
